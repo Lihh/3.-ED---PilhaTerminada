9bcce3f1a59ca53a9126a5d3337b91ac18ead2cb
Lidia Chou <lidia.chou94@gmail.com>
2015-02-06 11:09:44 -0200

    Initial Commit

---
+++ ExercicioPillha/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ExercicioPillha
+//
+//  Created by Lidia Chou on 2/6/15.
+//  Copyright (c) 2015 Lidia Chou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
